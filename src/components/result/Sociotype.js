92ac4f07300b4930394a1975ec926000d7251070
kartikeyachaauhan <kc1.408596@gmail.com>
2020-02-02 05:50:37 +0530

    formatting

--- src/components/result/Sociotype.js
+++ src/components/result/Sociotype.js
@@ -83,10 +83,8 @@ class Sociotype extends Component {
     return (
       <IEI
         title={'Introvert | Intuitive | Ethics | Rational'}
-        content={`Intuitive Ethical Introvert\n
-        STRENGTHS: Considerate, tactful, gentle person.\n
-        WEAKNESSES: Intrinsically vulnerable, contradictory. Somewhat unsure of himself. Prone to experiencing doubts and fluctuations in arriving at a decision\n
-        RECOMMENDATIONS: Best if occupation relates to humanities. Can work well anywhere where he is required to elegantly communicate with a small group of people. Recommended professions include translator, librarian, writer, journalist, editor, may realize themselves in the field of arts as critics, writers, and poets.\n`}
+        content={`
+        STRENGTHS: Considerate, tactful, gentle person.\nWEAKNESSES: Intrinsically vulnerable, contradictory. Somewhat unsure of himself. Prone to experiencing doubts and fluctuations in arriving at a decision\nRECOMMENDATIONS: Best if occupation relates to humanities. Can work well anywhere where he is required to elegantly communicate with a small group of people. Recommended professions include translator, librarian, writer, journalist, editor, may realize themselves in the field of arts as critics, writers, and poets.\n`}
         onBackClick={this.onIEI_click}
       />
     )
